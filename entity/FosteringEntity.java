320d1dd1540e36095f667dd9b79c0d1514580094
Gabriel Villarreal <71571648+villarr@users.noreply.github.com>
2021-08-27 02:02:13 -0700

    Updated Entities

--- entity/FosteringEntity.java
+++ entity/FosteringEntity.java
@@ -1,5 +1,50 @@
 package entity;
 
+import java.sql.Date;
+
 public class FosteringEntity {
+	private int fostering_id;
+	private int pet_id;
+	private int volunteer_id;
+	private java.sql.Date foster_date;
+
+public FosteringEntity(int fostering_id, int pet_id, int volunteer_id, Date foster_date) {
+	this.fostering_id = fostering_id;
+	this.pet_id = pet_id;
+	this.volunteer_id = volunteer_id;
+	this.foster_date = foster_date;
+}
+
+public int getFostering_id() {
+	return fostering_id;
+}
+
+public void setFostering_id(int fostering_id) {
+	this.fostering_id = fostering_id;
+}
+
+public int getPet_id() {
+	return pet_id;
+}
+
+public void setPet_id(int pet_id) {
+	this.pet_id = pet_id;
+}
+
+public int getVolunteer_id() {
+	return volunteer_id;
+}
+
+public void setVolunteer_id(int volunteer_id) {
+	this.volunteer_id = volunteer_id;
+}
+
+public java.sql.Date getFoster_date() {
+	return foster_date;
+}
+
+public void setFoster_date(java.sql.Date foster_date) {
+	this.foster_date = foster_date;
+}
 
 }
--- entity/ShelterEntity.java
+++ entity/ShelterEntity.java
@@ -1,5 +1,49 @@
 package entity;
 
 public class ShelterEntity {
+	private int location_id;
+	private String clinic_onsite;
+	private int number_of_cats;
+	private int number_of_dogs;
+	
+	public ShelterEntity(int location_id, String clinic_onsite, int number_of_cats, int number_of_dogs) {
+		
+		this.location_id = location_id;
+		this.clinic_onsite = clinic_onsite;
+		this.number_of_cats = number_of_cats;
+		this.number_of_dogs = number_of_dogs;
+	}
 
+	public int getLocation_id() {
+		return location_id;
+	}
+
+	public void setLocation_id(int location_id) {
+		this.location_id = location_id;
+	}
+
+	public String getClinic_onsite() {
+		return clinic_onsite;
+	}
+
+	public void setClinic_onsite(String clinic_onsite) {
+		this.clinic_onsite = clinic_onsite;
+	}
+
+	public int getNumber_of_cats() {
+		return number_of_cats;
+	}
+
+	public void setNumber_of_cats(int number_of_cats) {
+		this.number_of_cats = number_of_cats;
+	}
+
+	public int getNumber_of_dogs() {
+		return number_of_dogs;
+	}
+
+	public void setNumber_of_dogs(int number_of_dogs) {
+		this.number_of_dogs = number_of_dogs;
+	}
+	
 }
