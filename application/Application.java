40693a1c6f9d03887c56b35ccadcd4835352d244
Gabriel Villarreal <71571648+villarr@users.noreply.github.com>
2021-08-27 02:42:14 -0700

    Added menu functionality - need to make data conn

--- application/Application.java
+++ application/Application.java
@@ -1,5 +1,9 @@
 package application;
 
 public class Application {
-
+	
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.start();
+	}
 }
--- application/Menu.java
+++ application/Menu.java
@@ -1,5 +1,67 @@
 package application;
 
-public class Menu {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class Menu {
+	private Scanner scanner = new Scanner(System.in);
+	private List<String> options = Arrays.asList(
+			"Dislay Animals",
+			"Display Volunteers",
+			"Display Fostering",
+			"Display Shelter",
+			"Create Animals",
+			"Delete Animals",
+			"Create Volunteers",
+			"Delete Volunteers"
+			);
+	
+	
+	public void start() {
+		String selection = "";
+		
+		do {
+			printMenu();
+			selection = scanner.nextLine();
+			
+			if (selection.equals("1")) {
+				//displayAnimals();
+				
+			} else if (selection.equals("2")) {
+				//displayVolunteers();
+				
+			} else if (selection.equals("3")) {
+				//displayFostering();
+				
+			} else if (selection.equals("4")) {
+				//displayShelter();
+				
+			} else if (selection.equals("5")) {
+				//createAnimals();
+				
+			} else if (selection.equals("6")) {
+				//deleteAnimals();
+				
+			} else if (selection.equals("7")) {
+				//createVolunteers();
+				
+			} else if (selection.equals("7")) {
+				//deleteVolunteers();
+			}
+			
+		} while (!selection.equals("-1"));
+			System.out.println("Press enter to continue.....");
+			scanner.nextLine();
+			
+	}	
+
+
+
+private void printMenu () {
+	System.out.println("Welcome to the Animal Shelter Database. Please make a selection........");
+	for (int i = 0; i < options.size(); i ++) {
+		System.out.println(i + 1 + ") " + options.get(i));
+	}
+}
 }
